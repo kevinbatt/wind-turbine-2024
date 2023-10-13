31008f100b744b0a48a830221812dc6789ee6268
kevinbatt <mrkevinbat@gmail.com>
2023-10-13 11:44:02 -0400

    hopefully fixed it

--- serialcontrolfortesting_copy_/serialcontrolfortesting_copy_.ino
+++ serialcontrolfortesting_copy_/serialcontrolfortesting_copy_.ino
@@ -1,57 +1,94 @@
 #include <Servo.h> //include servo library
 Servo myservo;//create object for servo1
-
+Servo myservo2; 
 
 int pos = 10;//initialize pos for servo 1
-char ch = Serial.read();
+int pos2 = 10;
+char ch = Serial.readString().toInt();
 bool lastPos = pos; 
+bool lastPos2 = pos2;
+
 
 
 
 void setup() {
   Serial.begin(9600);
-  while(! Serial);
-  Serial.println("enter +F to move servo 1 postive +50, enter -F to move -50, enter +B to move servo 1 +10, - B to move -10.enter +K to move servo 2 +50 and -K to move -50, enter +t to move servo 2 +10 and -T to move -10. Enter R to reset.");
   myservo.attach(8);
+  myservo2.attach(9);
   pos = 10;
+  pos2 = 10;
   myservo.write(pos);
+  myservo2.write(pos2);
  
 
 
 }
 
 void loop() {
-  if(Serial.available()){
-    if(ch == 'f' || ch == 'F'){
+  Serial.println("enter value from 1 to 12 to move servo 1 or 2:");
+  while(Serial.available() == 0){}
+    ch = Serial.readString().toInt();
+    pos = 10;
+    pos2 = 10;
+    
+
+    if(ch == '1' ){
       pos = lastPos + 50;
       myservo.write(pos);
 
     }
-    if(ch == 'b' || ch == 'B'){
+    if(ch == '2' ){
       pos = lastPos - 50;
       myservo.write(pos);
 
 
     }
-    if(ch == 'w' || ch == 'W'){
+    if(ch == '3' ){
       pos = lastPos + 10;
       myservo.write(pos);
 
     }
-    if(ch == 'd'|| ch == 'D'){
+    if(ch == '4'){
       pos = lastPos - 10;
       myservo.write(pos);
 
     }
-    if(ch == 'y' || ch == 'Y'){
+    if(ch == '5' ){
       pos = lastPos + 1;
       myservo.write(pos);
     }
-    if(ch == 'p' || ch == 'P'){
+    if(ch == '6' ){
       pos = lastPos - 1;
       myservo.write(pos);
     }
-  }
+    if(ch == '7'){
+      pos2 = lastPos2 + 50;
+      myservo2.write(pos2);
+
+    }
+    if(ch == '8'){
+      pos2 = lastPos2 - 50;
+      myservo2.write(pos2);
+    }
+    if(ch == '9'){
+      pos2 = lastPos2 + 10;
+      myservo2.write(pos2);
+
+    }
+    if(ch == '10'){
+      pos2 = lastPos2 - 10;
+      myservo2.write(pos2);
+    }
+    if(ch == '11'){
+      pos2 = lastPos2 + 1;
+      myservo2.write(pos2);
+
+    }
+    if(ch == '12'){
+      pos2 = lastPos2 - 1;
+      myservo2.write(pos2);
+    }
+  lastPos = pos;
+  lastPos2= pos2;
+  
 } 
-
-
