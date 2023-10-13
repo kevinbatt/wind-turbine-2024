c9632f9494084227f967a98e87630f8815e42b6a
kevinbatt <mrkevinbat@gmail.com>
2023-10-13 09:45:08 -0400

    im not sure why it was not working so i made it to control only one actuator at a time, hopefully this one will work instead, i move char ch tobe global and i made a boolean function called lastPos

--- serialcontrolfortesting_copy_/serialcontrolfortesting_copy_.ino
+++ serialcontrolfortesting_copy_/serialcontrolfortesting_copy_.ino
@@ -1,9 +1,10 @@
 #include <Servo.h> //include servo library
 Servo myservo;//create object for servo1
-Servo myservo2;//create object for servo2
 
-int pos = 0;//initialize pos for servo 1
-int pos2 = 0;//initialize pos for servo 2
+
+int pos = 10;//initialize pos for servo 1
+char ch = Serial.read();
+bool lastPos = pos; 
 
 
 
@@ -12,75 +13,45 @@ void setup() {
   while(! Serial);
   Serial.println("enter +F to move servo 1 postive +50, enter -F to move -50, enter +B to move servo 1 +10, - B to move -10.enter +K to move servo 2 +50 and -K to move -50, enter +t to move servo 2 +10 and -T to move -10. Enter R to reset.");
   myservo.attach(8);
-  myservo2.attach(9);
+  pos = 10;
+  myservo.write(pos);
+ 
 
 
 }
 
 void loop() {
   if(Serial.available()){
-    char ch = Serial.read();
-    if(ch == '+F'|| ch == '+f'){
-      pos = pos+50;
+    if(ch == 'f' || ch == 'F'){
+      pos = lastPos + 50;
       myservo.write(pos);
-      delay(1000);
 
     }
-    if(ch == '-F'|| ch == '-f'){
-      pos = pos-50;
+    if(ch == 'b' || ch == 'B'){
+      pos = lastPos - 50;
       myservo.write(pos);
-      delay(1000);
+
 
     }
-    if(ch == '+B' || ch == '+b'){
-      pos = pos+10;
+    if(ch == 'w' || ch == 'W'){
+      pos = lastPos + 10;
       myservo.write(pos);
-      delay(1000);
-    }
-    if(ch == '-B' || ch == '-b'){
-    pos = pos-10;
-    myservo.write(pos);
-    delay(1000);
 
     }
-    if(ch == '+k' || '+k'){
-      pos2 = pos2+50;
-      myservo2.write(pos2);
-      delay(1000);
+    if(ch == 'd'|| ch == 'D'){
+      pos = lastPos - 10;
+      myservo.write(pos);
 
     }
-    if(ch == '-K' || '-k'){
-      pos2 = pos2-50;
-      myservo2.write(pos2);
-      delay(1000);
+    if(ch == 'y' || ch == 'Y'){
+      pos = lastPos + 1;
+      myservo.write(pos);
     }
-   if(ch == '+T' || '+t'){
-    pos2 = pos2+10;
-    myservo2.write(pos2);
-    delay(1000);
-   }
-   if(ch == '-T' || ch == '-t'){
-    pos2 = pos2-10;
-    myservo2.write(pos2);
-    delay(1000);
-   }
-   if(ch == 'R' || ch == 'r'){
-    pos = 0;
-    pos2 = 0;
-    myservo.write(pos);
-    myservo2.write(pos2);
-    delay(1000);
-   }
-   else{
-    pos = 0;
-    pos2 = 0; 
-    myservo.write(pos);
-    myservo2.write(pos2);
-    
-   }
-   
+    if(ch == 'p' || ch == 'P'){
+      pos = lastPos - 1;
+      myservo.write(pos);
+    }
   }
+} 
 
- 
-}
 
