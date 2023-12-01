9f90ee44d49c63a311d89149a71986d668f2aaea
kevinbatt <mrkevinbat@gmail.com>
2023-11-30 22:39:43 -0500

    added more code for testing

--- serialcontrolsimple/serialcontrolsimple.ino
+++ serialcontrolsimple/serialcontrolsimple.ino
@@ -1,3 +1,12 @@
+//This is the code that i showed during our meeting. To wire the driver for the acctuator to the arduino, 
+//attach one of the - terminals of the driver to a GND pin on the arduino.
+//Then attach the RC pin of the driver to pin 9 of the arduino. Then connect the other - terminal and the + terminal of the driver to a 12V supply. 
+//There is an ac to 12VDC wall converter in one of our boxes. This works fine. 
+//To use the code, first upload it to the baord then go to tools and select serial monitor. 
+//from the serial monitor you can type what position you want the acctuator to go to. 
+
+
+
 #include<Servo.h>
 Servo myservo;
 
@@ -5,7 +14,7 @@ int pos;
 
 void setup(){
   Serial.begin(9600);
-  myservo.attach(8);
+  myservo.attach(9);
   
 
 
